812e05d00405297a9b563ebd02c61c5691c46e0e
Noam Mirjani <Noamirjani@gmail.com>
2023-07-30 00:13:00 +0300

    done stage 3

--- main.c
+++ main.c
@@ -1,11 +1,22 @@
+/**
+ *   This program manage a school database.
+ *   It can add, delete, update, and print students.
+ *   It can also backup the database to a file, and restore it from a file.
+ *
+ *   Notice: there is use of the openssl library in this program.
+ *   To install openssl in linux: sudo apt-get install libssl-dev
+ *   To install openssl in mac: brew install openssl
+ *
+ *   To compile: gcc crypt.c -o crypt -lcrypto
+ *   To run: ./crypt
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <openssl/conf.h>
 #include <openssl/evp.h>
 #include <openssl/err.h>
-#include <string.h>
-
 
 //--------------------------- defines ---------------------------//
 #define NUM_COURSES 10
@@ -14,11 +25,8 @@
 #define NUM_LEN 15
 #define CELL_LEN 11
 #define TOP_STUDENTS 10
-#define STUDENTS_FILE "students.txt"
 #define OUTPUT_FILE "backupDB.txt"
 #define ENCRYPTED_FILE "encrypted.txt"
-#define DECRYPTED_FILE "decrypted.txt"
-
 
 //--------------------------- structs ---------------------------//
 typedef struct Student Student;
@@ -46,7 +54,6 @@ const char* const courses_names[NUM_COURSES] = {
 
 //--------------------------- global variables ---------------------------//
 static School school;
-
 
 //--------------------------- memory functions ---------------------------//
 void freeSchool(){
@@ -96,6 +103,50 @@ Student* allocateStudent(){
     return stud;
 }
 
+FILE* fileopen(const char* filename, const char* mode){
+    FILE* fp;
+    fp = fopen(filename, mode);
+    if(fp == NULL){
+        programFailed("Error opening file\n");
+    }
+    return fp;
+}
+
+void handleErrors(void)
+{
+    ERR_print_errors_fp(stderr);
+    abort();
+}
+
+int decryptLine(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
+                unsigned char *iv, unsigned char *plaintext)
+{
+    EVP_CIPHER_CTX *ctx;
+    int len, plaintext_len;
+
+    // Create and initialise the context
+    if(!(ctx = EVP_CIPHER_CTX_new()))
+        handleErrors();
+
+    // Initialise the decryption operation.
+    if(1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
+        handleErrors();
+
+    // Provide the message to be decrypted, and obtain the plaintext output.
+    if(1 != EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len))
+        handleErrors();
+    plaintext_len = len;
+
+    // Finalise the decryption. Further plaintext bytes may be written at this stage.
+    if(1 != EVP_DecryptFinal_ex(ctx, plaintext + len, &len))
+        handleErrors();
+    plaintext_len += len;
+
+    // Clean up
+    EVP_CIPHER_CTX_free(ctx);
+
+    return plaintext_len;
+}
 
 //--------------------------- utility functions --------------------------//
 void checkRangeInput(const int var, const int max){
@@ -104,15 +155,6 @@ void checkRangeInput(const int var, const int max){
         sprintf(message, "Invalid input %d is not in the valid range {1, %d}", var, max);
         programFailed(message);
     }
-}
-
-FILE* fileopen(const char* filename, const char* mode){
-    FILE* fp;
-    fp = fopen(filename, mode);
-    if(fp == NULL){
-        programFailed("Error opening file\n");
-    }
-    return fp;
 }
 
 void clearStdin(){
@@ -161,7 +203,6 @@ double calcStudAvg(const Student* stud){
     for(int i = 0; i < NUM_COURSES; i++){
         sum += stud->courses_grades[i];
     }
-    if(NUM_COURSES == 0) return 0;
     return sum / NUM_COURSES;
 }
 
@@ -203,17 +244,28 @@ void parseStudentFromFileLine(char* line){
 }
 
 void initDB(){
-    FILE* fp = fileopen(STUDENTS_FILE, "r");
+    unsigned char ciphertext[128];
+    unsigned char decryptedText[128];
+    int ciphertext_len;
 
-    char *line = NULL;
-    size_t len = 0;
+    unsigned char key[] = { 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37,
+                            0x38, 0x39, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35,
+                            0x36, 0x37, 0x38, 0x39, 0x30, 0x31, 0x32, 0x33,
+                            0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x30, 0x31 };
 
-    while (getline(&line, &len, fp) != -1) {
-        parseStudentFromFileLine(line);
+    /* A 128 bit IV */
+    unsigned char iv[] = { 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37,
+                           0x38, 0x39, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35 };
+
+    FILE* encryptedFile = fileopen(ENCRYPTED_FILE, "rb");
+    while (fread(&ciphertext_len, sizeof(int), 1, encryptedFile) > 0) {
+        fread(ciphertext, sizeof(unsigned char), ciphertext_len, encryptedFile);
+        int decryptedTextLen = decryptLine(ciphertext, ciphertext_len, key, iv, decryptedText);
+        decryptedText[decryptedTextLen] = '\0';
+        parseStudentFromFileLine((char*)decryptedText);
     }
 
-    free(line);
-    fclose(fp);
+    fclose(encryptedFile);
 }
 
 void printDB(FILE* fp){
@@ -250,7 +302,6 @@ Student* searchInClass(Student* class, const char fname[NUM_LEN], const char lna
     }
     return stud;
 }
-
 
 //--------------------------- menu functions -----------------------------//
 void printMenu(){
@@ -470,136 +521,11 @@ void menu(){
     }while(task != 0);
 }
 
-
-//--------------------------- encryption & decryption ----------------------//
-void handleErrors(void)
-{
-    ERR_print_errors_fp(stderr);
-    abort();
-}
-
-int encryptLine(unsigned char *plaintext, int plaintext_len, unsigned char *key,
-            unsigned char *iv, unsigned char *ciphertext)
-{
-    EVP_CIPHER_CTX *ctx;
-    int len, ciphertext_len;
-
-    // Create and initialise the context
-    if(!(ctx = EVP_CIPHER_CTX_new()))
-        handleErrors();
-
-    // Initialise the encryption operation.
-    if(1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
-        handleErrors();
-
-    // Provide the message to be encrypted, and obtain the encrypted output.
-    if(1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len))
-        handleErrors();
-    ciphertext_len = len;
-
-    // Handle any remaining data and finalize the encryption.
-    if(1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len))
-        handleErrors();
-    ciphertext_len += len;
-
-    /* Clean up */
-    EVP_CIPHER_CTX_free(ctx);
-    return ciphertext_len;
-}
-
-void encrypt(unsigned char key[], unsigned char iv[]) {
-    FILE *plaintextFile = fileopen(STUDENTS_FILE, "r");
-    FILE *encryptedFile = fileopen(ENCRYPTED_FILE, "wb");
-
-    unsigned char ciphertext[128];
-    char plaintext[128]; // Changed from unsigned char to char
-
-    while (fgets(plaintext, sizeof plaintext, plaintextFile) != NULL) {
-        int ciphertext_len = encryptLine((unsigned char *)plaintext, strlen(plaintext), key, iv, ciphertext);
-
-        // write the length of the encrypted message to file
-        fwrite(&ciphertext_len, sizeof(int), 1, encryptedFile);
-        // Write the encrypted message to file
-        fwrite(ciphertext, sizeof(unsigned char), ciphertext_len, encryptedFile);
-    }
-
-    fclose(plaintextFile);
-    fclose(encryptedFile);
-}
-
-int decryptLine(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
-                   unsigned char *iv, unsigned char *plaintext)
-{
-    EVP_CIPHER_CTX *ctx;
-    int len, plaintext_len;
-
-    // Create and initialise the context
-    if(!(ctx = EVP_CIPHER_CTX_new()))
-        handleErrors();
-
-    // Initialise the decryption operation.
-    if(1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
-        handleErrors();
-
-    // Provide the message to be decrypted, and obtain the plaintext output.
-    if(1 != EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len))
-        handleErrors();
-    plaintext_len = len;
-
-    // Finalise the decryption. Further plaintext bytes may be written at this stage.
-    if(1 != EVP_DecryptFinal_ex(ctx, plaintext + len, &len))
-        handleErrors();
-    plaintext_len += len;
-
-    // Clean up
-    EVP_CIPHER_CTX_free(ctx);
-
-    return plaintext_len;
-}
-
-void decrypt(unsigned char key[], unsigned char iv[]) {
-
-    FILE *encryptedFile = fileopen(ENCRYPTED_FILE, "rb");
-    FILE *decryptedFile = fileopen(DECRYPTED_FILE, "w");
-
-    unsigned char ciphertext[128];
-    unsigned char decryptedText[128];
-    int ciphertext_len;
-
-    while (fread(&ciphertext_len, sizeof(int), 1, encryptedFile) > 0) {
-        fread(ciphertext, sizeof(unsigned char), ciphertext_len, encryptedFile);
-        int decryptedTextLen = decryptLine(ciphertext, ciphertext_len, key, iv, decryptedText);
-        decryptedText[decryptedTextLen] = '\0';
-        fwrite(decryptedText, sizeof(char), decryptedTextLen, decryptedFile);
-    }
-
-    fclose(encryptedFile);
-    fclose(decryptedFile);
-}
-
-
-void prepareEncryption(){
-    /* A 256 bit key */
-    unsigned char key[] = { 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37,
-                            0x38, 0x39, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35,
-                            0x36, 0x37, 0x38, 0x39, 0x30, 0x31, 0x32, 0x33,
-                            0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x30, 0x31 };
-
-    /* A 128 bit IV */
-    unsigned char iv[] = { 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37,
-                           0x38, 0x39, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35 };
-
-    encrypt(key, iv);
-    decrypt(key, iv);
-}
-
 int main(){
 
-   // initDB();
-   //  printDB(stdout);
-   // menu();
-   // freeSchool();
-    prepareEncryption();
+    initDB();
+    menu();
+    freeSchool();
 
     return 0;
 }
